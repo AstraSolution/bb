2127eb259af77a9204041ee51dfe02850fd5ff7b
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-03-12 12:56:22 +0600

    update iterest hook

--- src/Hooks/SuggesteBooks/useInterestedBooks.jsx
+++ src/Hooks/SuggesteBooks/useInterestedBooks.jsx
@@ -3,12 +3,13 @@ import useAxiosPublic from "../Axios/useAxiosPublic";
 
 
 const useInterestedBooks = () => {
-    const axiosPublic = useAxiosPublic()
+    const axiosPublic = useAxiosPublic();
+    const email = localStorage.getItem("email")
     
     const { data: interestedBooks, isLoading, refetch} = useQuery({
         queryKey: ["interestedBooks"],
         queryFn: async() => {
-            const res  = await axiosPublic.get(`/api/v1/suggestedBooks`);
+            const res  = await axiosPublic.get(`/api/v1/suggestedBooks?email=${email}`);
             return res.data;
         }
     })
